32510f5a4af4098f8e068fc1beece56e6e31b987
Arshad <arshad.syed.anwar@gmail.com>
2022-05-22 00:47:01 +0100

    heroku ready

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
