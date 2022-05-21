0032bcc5f341ff773bd67938f32a34009fa3e2da
Arshad <arshad.syed.anwar@gmail.com>
2022-05-22 00:25:40 +0100

    project dependencies setup

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = 5000 || process.env.PORT;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello')
